42ae0ff487d1cf87a5b53dd899f0a90b1e677ca9
nicolaifolberth <127974319+nicolaifolberth@users.noreply.github.com>
2023-05-22 18:42:51 +0200

    Update DateiManager.java

---
+++ DateiManager.java
@@ -0,0 +1,10 @@
+public class DateiManager{
+  public void verarbeiteDatei(String art, int schluessel, int a, int b, int m, String ){
+  }
+  private byte leseByte(){
+    return kodiertesByte;
+  }
+  private void schreibeByte(byte kodiertesByte){
+    
+  }
+ }
