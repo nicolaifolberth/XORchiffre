e593ec35d8b56642aad3ade45a0f88a92762781c
Nicolai Folberth <nicolai.folberth@gmail.com>
2023-05-29 15:45:43 +0200

    Klasse DateiManager implementiert, Klasse Kodierung implementiert, Klasse Verschlüsselungssequenz implementiert

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ public class Main{
 
   public static void main(String[] args){
     DateiManager testManager = new DateiManager();
-    //testManager.verarbeiteDatei( "Chiffrierung", 3, 421, 54773, 259200, "Gedicht.txt","chiffriertesGedicht.txt");
-    //testManager.verarbeiteDatei( "Dechiffrierung", 3, 421, 54773, 259200, "dechiffriertesGedicht.txt","chiffriertesGedicht.txt");
+    testManager.verarbeiteDatei( "Chiffrierung", 3, 421, 54773, 259200, "Gedicht.txt","chiffriertesGedicht.txt");
+    testManager.verarbeiteDatei( "Dechiffrierung", 3, 421, 54773, 259200, "dechiffriertesGedicht.txt","chiffriertesGedicht.txt");
 
-    //testManager.verarbeiteDatei( "Chiffrierung", 3, 38777, 29573, 139968, "Gedicht.txt","chiffriertesGedichtParameterZwei.txt");
+    testManager.verarbeiteDatei( "Chiffrierung", 3, 38777, 29573, 139968, "Gedicht.txt","chiffriertesGedichtParameterZwei.txt");
     testManager.verarbeiteDatei( "Dechiffrierung", 3, 38777, 29573, 139968, "dechiffriertesGedichtParameterZwei.txt","chiffriertesGedichtParameterZwei.txt");
   }
 
