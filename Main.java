e577fe9470beddf66d13c99ea5fd137eb015142c
hxena <134312865+hxena@users.noreply.github.com>
2023-06-02 09:51:55 +0200

    added Javadoc/comment

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
+/**
+ * Die Klasse Main stellt den Einstiegspunkt des Programms dar.
+ * Sie enthält die main-Methode, in der die Programmausführung startet.
+ */
 public class Main{
-
+  /**
+   * Die main-Methode, in der die Verarbeitung von Dateien mit verschiedenen Parametern getestet wird.
+   */
   public static void main(String[] args){
-    DateiManager testManager = new DateiManager();
-    testManager.verarbeiteDatei( "Chiffrierung", 3, 421, 54773, 259200, "Gedicht.txt","chiffriertesGedicht.txt");
-    testManager.verarbeiteDatei( "Dechiffrierung", 3, 421, 54773, 259200, "dechiffriertesGedicht.txt","chiffriertesGedicht.txt");
-
-    testManager.verarbeiteDatei( "Chiffrierung", 3, 38777, 29573, 139968, "Gedicht.txt","chiffriertesGedichtParameterZwei.txt");
-    testManager.verarbeiteDatei( "Dechiffrierung", 3, 38777, 29573, 139968, "dechiffriertesGedichtParameterZwei.txt","chiffriertesGedichtParameterZwei.txt");
+	
+	DateiManager testManager = new DateiManager();
+	// Testen der Verarbeitung mit verschiedenen Parametern
+	testManager.verarbeiteDatei( "Chiffrierung", 3, 421, 54773, 259200, "Gedicht.txt","chiffriertesGedicht.txt");
+	testManager.verarbeiteDatei( "Dechiffrierung", 3, 421, 54773, 259200, "dechiffriertesGedicht.txt","chiffriertesGedicht.txt");
+	testManager.verarbeiteDatei( "Chiffrierung", 3, 38777, 29573, 139968, "Gedicht.txt","chiffriertesGedichtParameterZwei.txt");
+	testManager.verarbeiteDatei( "Dechiffrierung", 3, 38777, 29573, 139968, "dechiffriertesGedichtParameterZwei.txt","chiffriertesGedichtParameterZwei.txt");
   }
 
 }
